9f7ee2f9ff11cd0090769b8e824bb452bbc19e79
BeatrizCercal <biaccachoeira@gmail.com>
2026-06-16 19:00:13 -0300

    feat: desafio 2

---
+++ desafio_bugs2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int aura
+    char nome[50]
+    char profissao[50];
+ 
+    printf("Qual e a aura deste trabalho? ")
+    scanff("%d", &aura);
+ 
+    printf("Digite seu nome: ");
+    scanf("%s", nome)
+ 
+    printf("Digite a profissao que voce quer ter: ";
+    scanf("%s", profissao);
+ 
+    printf("Ola, %s!\n", nomes);
+ 
+    printf("Voce quer ser %s\n", profissaoDesejada);
+ 
+    if (aura >= 50)
+        printf("Trabalho aprovado!\n");
+        printf("Parabens!\n")
+ 
+    printf("Aura deste trabalho: %d\n", auras);
+ 
+    return 0
+} 

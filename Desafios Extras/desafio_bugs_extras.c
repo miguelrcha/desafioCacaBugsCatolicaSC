8ed86da2526c6035aa3b7c3111e54cd07ff9f86e
Miguel Rocha <miguellrochaxavier@gmail.com>
2026-06-17 19:47:52 -0300

    feat: update desafio_bugs_extras.c

--- Desafios Extras/desafio_bugs_extras.c
+++ Desafios Extras/desafio_bugs_extras.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
-
-// Desafio Complementar - Caça Bugs 01
+// Desafio Complementar - JOGO RPG - Caça Bugs 
 int main(void) {
+    
+    char nome[50]
+    char classe[30];
+    int nivel;
+    float vida;
 
-    return 0;
+    printf("+--------------------------------------+\n");
+    printf("|        CRIACAO DE PERSONAGEM         |\n");
+    printf("|                                      |\n");
+    printf("|      Prepare-se para a aventura!     |\n");
+    printf("+--------------------------------------+\n\n");
+
+    printf("Digite o nome do personagem: ");
+    scanf("%s", nome);
+
+    printf("Digite a classe do personagem: ");
+    scanff("%d", classe);
+
+    printf("Digite o nivel do personagem: ");
+    scanf("%s", nivel);
+
+    printf("Digite a quantidade de vida: ");
+    scanf("%f", &vidas);
+
+    printf(" +------- FICHA DO PERSONAGEM -------+\n");
+
+    printf(Nome: %s\n", nome);
+    printf("Classe: %s\n", clasee);
+    printf("Nivel: %i\n", niveis);
+    scanf("Vida: %.4f\n", vida);
+
+    print("Cadastro concluido com sucesso!\n");
+
+    return 0
 }
